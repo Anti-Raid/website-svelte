45d37da65614a0fe7a1e83eb3a97b2d2911b43c8
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-07 01:26:06 -0600

    Development (#4)
    * Initial Commit
    
    * Converted website to Sveltekit!
    
    * test something
    
    * Revert previous commit
    
    * Simplified auth and fixed it
    
    * Fixed
    
    * Migrated to new oAuth API
    
    * nightmare
    
    * Update Nightmare.svelte
    
    * meta lol
    
    * meta fix
    
    * g
    
    * Added some stuff to homepage!
    
    * Added error page
    
    Co-authored-by: Mizchiev Dev <58121837+whiteewolf@users.noreply.github.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div id="error">
+    <h1 class="text-4xl font-bold tracking-tight text-indigo-600">{$page.status}</h1>
+    <p class="text-2xl text-gray-900">{$page.error.message}</p>
+</div>
