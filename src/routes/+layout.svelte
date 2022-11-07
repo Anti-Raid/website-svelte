413ef71c51b1b3274bda7e6a15a200973c9ef62d
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-07 00:02:07 -0600

    Development (#3)
    * Initial Commit
    
    * Converted website to Sveltekit!
    
    * test something
    
    * Revert previous commit
    
    * Simplified auth and fixed it
    
    * Fixed
    
    * Migrated to new oAuth API
    
    * nightmare
    
    * Update Nightmare.svelte
    
    * meta lol
    
    * meta fix
    
    * g
    
    * Added some stuff to homepage!
    
    Co-authored-by: Mizchiev Dev <58121837+whiteewolf@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,17 @@
 	import 'sweetalert2/dist/sweetalert2.min.css';
 	import Header from '../components/Header.svelte';
 
+    import Nightmare from '../components/Nightmare.svelte';
+
 	export let data;
 </script>
 
 <Header user={data.user || null} />
+
+<Nightmare
+    Title="Home"
+    Description="Nightmare"
+/>
 
 <div class="m-4">
 	<slot />
