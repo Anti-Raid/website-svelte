abbda44b2fb77478b7c28e0182520d82398da42d
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-06 19:14:59 -0600

    nightmare

---
+++ src/components/Nightmare.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Logo from "$lib/assets/logo.webp";
+
+    export let Title: string;
+    export let Description: string;
+    export let Image: string = Logo;
+</script>
+
+<svelte:head>
+    <title>{Title}</title>
+    <meta property="og:title" content="{Title} - AntiRaid" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://antiraid.xyz" />
+    <meta property="og:image" content={Image} />
+    <meta property="og:description" content={Description} />
+</svelte:head>
