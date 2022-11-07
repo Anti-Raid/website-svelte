32256073f1be489d2f170a25f3933a343b2536a7
selectdev <66577268+selectdev@users.noreply.github.com>
2022-11-06 22:15:11 -0600

    meta lol

--- src/components/Nightmare.svelte
+++ src/components/Nightmare.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     export let Title: string;
     export let Description: string;
-    export let Image: string = "";
+    export let Image: string = "/logo.webp";
 </script>
 
 <svelte:head>
     <title>{Title}</title>
-    <meta property="og:title" content="{Title} - AntiRaid" />
+    <meta property="og:title" content={Title} />
+    <meta property="og:site_title" content="AntiRaid" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://antiraid.xyz" />
     <meta property="og:image" content={Image} />
