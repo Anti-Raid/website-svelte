2344098e80249a3c0ade80d57b59a3b19ff02a3e
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-11 09:20:49 -0600

    Made it so it only shows guilds that you have permissions in

--- src/routes/invite/+page.svelte
+++ src/routes/invite/+page.svelte
@@ -4,8 +4,18 @@
 
 <div class="servers">
     {#each data.user.guilds as guild}
-        <div class="guild">
-            <h1>{guild.name}</h1>
-        </div>
+        {#if guild.owner === true}
+            <div class="guild">
+                <h1>{guild.name} ({guild.id})</h1>
+            </div>
+        {:else if guild.permissions["Administrator"] === true}
+            <div class="guild">
+                <h1>{guild.name} ({guild.id})</h1>
+            </div>
+        {:else if guild.permissions["ManageGuild"] === true}
+            <div class="guild">
+                <h1>{guild.name} ({guild.id})</h1>
+            </div>
+        {/if}
     {/each}
 </div>
