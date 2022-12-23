610b347c41b5e6565f697dcb1791056a72d4788c
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-23 17:43:26 -0600

    Fixed background

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,19 +1,26 @@
 <script>
-    export let name;
-    export let title;
-    export let description;
-    export let image;
-    export let button;
+	export let name;
+	export let title;
+	export let description;
+	export let image;
+	export let button;
 </script>
 
-<div id={name} class="p-6 max-w-sm bg-white rounded-md border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <h5 class="mb-2 text-2xl font-bold tracking-light text-gray-900 dark:text-white">{title}</h5>
-    <img class="align-right" src={image} height="100px" width="100px" alt="Image" />
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+<div
+	id={name}
+	class="p-6 max-w-sm bg-white rounded-md border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+>
+	<h5 class="mb-2 text-2xl font-bold tracking-light text-gray-900 dark:text-white">{title}</h5>
+	<img class="align-right" src={image} height="100px" width="100px" alt="Image" />
+	<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
 
-    {#if button}
-        <button on:click={button.click} class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-emerald-700 rounded-md focus:ring-4 focus:outline-none focus:ring-emerald-300 dark:bg-emerald-700 dark:focus:ring-emerald-800">{button.name}</button>
-    {/if}
+	{#if button}
+		<button
+			on:click={button.click}
+			class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-emerald-700 rounded-md focus:ring-4 focus:outline-none focus:ring-emerald-300 dark:bg-emerald-700 dark:focus:ring-emerald-800"
+			>{button.name}</button
+		>
+	{/if}
 </div>
 
-<div class="p-3"></div>
+<div class="p-3" />
--- src/routes/invite/+page.svelte
+++ src/routes/invite/+page.svelte
@@ -1,23 +1,46 @@
 <script>
-    import Card from "../../components/Card.svelte";
-    export let data;
+	import Card from '../../components/Card.svelte';
+	export let data;
 </script>
 
 {#if data.user}
-    <div class="servers">
-        {#each data.user.guilds as guild}
-            {#if guild.owner === true}
-                <Card name="guild" title={guild.name} description="You are the owner of this server, you should have enough permissions to invite us into your server!" image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png" button={null} />
-            {:else if guild.permissions["Administrator"] === true}
-                <Card name="guild" title={guild.name} description="You have enough permissions to invite us to this server!" image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png" button={null} />
-            {:else if guild.permissions["ManageGuild"] === true}
-                <Card name="guild" title={guild.name} description="You have enough permissions to invite us to this server!" image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png" button={null} />
-            {:else}
-                <Card name="guild" title={guild.name} description="You do not have enough permissions to invite us to this server!" image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png" button={null} />
-            {/if}
-        {/each}
-    </div>
-
-    {:else}
-       <h2>You are not logged in. Please login to view this page.</h2>
+	<div class="servers">
+		{#each data.user.guilds as guild}
+			{#if guild.owner === true}
+				<Card
+					name="guild"
+					title={guild.name}
+					description="You are the owner of this server, you should have enough permissions to invite us into your server!"
+					image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png"
+					button={null}
+				/>
+			{:else if guild.permissions['Administrator'] === true}
+				<Card
+					name="guild"
+					title={guild.name}
+					description="You have enough permissions to invite us to this server!"
+					image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png"
+					button={null}
+				/>
+			{:else if guild.permissions['ManageGuild'] === true}
+				<Card
+					name="guild"
+					title={guild.name}
+					description="You have enough permissions to invite us to this server!"
+					image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png"
+					button={null}
+				/>
+			{:else}
+				<Card
+					name="guild"
+					title={guild.name}
+					description="You do not have enough permissions to invite us to this server!"
+					image="https://cdn.discordapp.com/icons/{guild.id}/{guild.icon}.png"
+					button={null}
+				/>
+			{/if}
+		{/each}
+	</div>
+{:else}
+	<h2>You are not logged in. Please login to view this page.</h2>
 {/if}
