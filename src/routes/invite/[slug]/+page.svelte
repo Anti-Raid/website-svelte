75380abe1200b836faedfccb87370ab41e71d55b
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-24 09:39:09 -0600

    Update +page.svelte

--- src/routes/invite/[slug]/+page.svelte
+++ src/routes/invite/[slug]/+page.svelte
@@ -1,8 +1,6 @@
 <script>
   /** @type {import('./$types').PageData} */
   export let data;
-  export let params;
 </script>
 
 <h2 class="text-white">{JSON.stringify(data)}</h2>
-<h2 class="text-white mt-3">{JSON.stringify(params)}</h2>
