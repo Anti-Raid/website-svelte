6c7dd0a11848f24bdf9f9257264a87d5ac84f44c
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-24 10:19:16 -0600

    Update +page.svelte

--- src/routes/invite/[slug]/+page.svelte
+++ src/routes/invite/[slug]/+page.svelte
@@ -3,7 +3,7 @@
   export let data;
   
   const CheckPerm = (permissions, owner) => {
-    if (permissions.includes("Administrator") || permissions.includes("ManageGuild") || owner === true) return true;
+    if (permissions.contains("Administrator") || permissions.contains("ManageGuild") || owner === true) return true;
     else return false;
   };
 
