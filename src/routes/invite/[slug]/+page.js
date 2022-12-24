45fd8873071279bd00db6a494486aeab2f28698b
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-24 10:41:12 -0600

    Update +page.js

--- src/routes/invite/[slug]/+page.js
+++ src/routes/invite/[slug]/+page.js
@@ -3,6 +3,6 @@ import { error } from '@sveltejs/kit';
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
   return {
-    guild_id: params.slug
+    slug: params.slug
   }
 }
