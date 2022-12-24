f3bb952aeb53b9fd453a72136eca31efd4498821
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-24 11:22:08 -0600

    Update +page.svelte

--- src/routes/invite/[slug]/+page.svelte
+++ src/routes/invite/[slug]/+page.svelte
@@ -7,28 +7,16 @@
     else return false;
   };
 
-  export let guild;
-  let finished = false;
-
-  data.user.guilds.forEach((i) => {
-    if (finished) return;
-
-    if (i.id === data.slug && CheckPerm(i.permissions, i.owner)) {
-       guild = i;
-       finished = true;
-    }
-    else return null;
-  });
+  export let guild = data.user.guilds.find((i) => i.id === data.slug && CheckPerm(i.permissions, i.owner));
 </script>
 
 {#if data.user}
    {#if guild}
       <h1 class="text-white">You have permissions to invite us to this guild.</h1>
-
-      {:else}
-        <h1 class="text-white">You don't have permissions to invite us to this guild.</h1>
+   {:else}
+      <h1 class="text-white">You don't have permissions to invite us to this guild.</h1>
    {/if}
 
    {:else}
-        <h1 class="text-white">You are not logged in. Please login and reload this page.</h1>
+      <h1 class="text-white">You are not logged in. Please login and reload this page.</h1>
 {/if}
