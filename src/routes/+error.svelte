fb80bb596cb904d495fed5277c1c91fca2ed403b
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-25 09:11:02 -0600

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,9 @@
 <script>
 	import { page } from '$app/stores';
+
+        export let subText;
+
+        if ($page.status === 400) subText = "But dont worry, you can find plenty of other things on our homepage.";
 </script>
 
 <section class="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
@@ -9,6 +13,7 @@
 				<span class="sr-only">Error</span>{$page.status}
 			</h2>
 			<p class="text-2xl font-semibold md:text-3xl">{$page.error.message}</p>
+                        <p class="mt-4 mb-8 dark:text-gray-400">{subText}</p>
 			<a rel="noopener noreferrer" href="/" class="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
 		</div>
 	</div>
