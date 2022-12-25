38ccac21a27c3c072d185b6ac80fe3ba92ea44f8
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-25 09:08:25 -0600

    Updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,14 @@
 	import { page } from '$app/stores';
 </script>
 
-<div class="items-center content-center" id="error">
-	<h1 class="text-6xl text-center font-bold tracking-tight text-indigo-600">{$page.status}</h1>
-	<p class="text-3xl text-center text-white">{$page.error.message}</p>
-</div>
+<section class="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+	<div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+		<div class="max-w-md text-center">
+			<h2 class="mb-8 font-extrabold text-9xl dark:text-gray-600">
+				<span class="sr-only">Error</span>{$page.status}
+			</h2>
+			<p class="text-2xl font-semibold md:text-3xl">{$page.error.message}</p>
+			<a rel="noopener noreferrer" href="/" class="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
+		</div>
+	</div>
+</section>
