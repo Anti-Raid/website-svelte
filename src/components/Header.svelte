0395bc1cdd983f5dbd64423dfdaffb4c0a99b677
selectdev <66577268+selectdev@users.noreply.github.com>
2022-12-25 19:27:04 -0600

    Fixed

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -59,7 +59,7 @@
 		}
 	};
 
-    const openProfileMenu = () => {
+	const openProfileMenu = () => {
 		const profileMenu = document.getElementById('profile_menu') as HTMLDivElement;
 		const className = profileMenu.className;
 
@@ -154,46 +154,48 @@
 				<div
 					class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
 				>
+					{#if user}
+						<button
+							type="button"
+							class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+							on:click={openNotificationPanel}
+						>
+							<span class="sr-only">View notifications</span>
+							<svg
+								class="h-6 w-6"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+								/>
+							</svg>
+						</button>
+
+						<div
+							class="absolute right-0 z-10 mt-2 w-48 origin-top-right invisible"
+							role="menu"
+							aria-orientation="vertical"
+							aria-labelledby="user-notifications-button"
+							tabindex="-1"
+							id="open_notifications"
+						>
+							{#each profileNavigation as item}
+								<a href={item.href} class="block px-4 py-2 text-sm text-gray-700">
+									{item.name}
+								</a>
+							{/each}
+						</div>
+					{/if}
+
 					<div class="relative ml-3">
 						{#if user}
-							<button
-								type="button"
-								class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                on:click={openNotificationPanel}
-                            >
-								<span class="sr-only">View notifications</span>
-								<svg
-									class="h-6 w-6"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-									/>
-								</svg>
-							</button>
-
-							<div
-								class="absolute right-0 z-10 mt-2 w-48 origin-top-right invisible"
-								role="menu"
-								aria-orientation="vertical"
-								aria-labelledby="user-notifications-button"
-								tabindex="-1"
-								id="open_notifications"
-							>
-								{#each profileNavigation as item}
-									<a href={item.href} class="block px-4 py-2 text-sm text-gray-700">
-										{item.name}
-									</a>
-								{/each}
-							</div>
-
 							<div>
 								<button
 									type="button"
