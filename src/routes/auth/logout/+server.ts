ade64930e09f0d14914fdabddbb4515c04851b00
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-14 19:24:12 -0600

    Update +server.ts

--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -2,6 +2,11 @@ import { error, redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ request, cookies, url }) {
-	cookies.delete('token');
+	cookies.set('token', "", {
+		path: '/',
+		secure: true,
+                expires: new Date()
+	});
+
 	throw redirect(307, '/');
 }
