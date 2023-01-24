2e0714aabf48888dcba2098eda4a81ded53c4449
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-24 17:53:43 -0600

    Fixed issue (most likely)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,17 @@
 	import '../app.css';
 	import 'sweetalert2/dist/sweetalert2.min.css';
 	import Header from '../components/Header.svelte';
-        import { browser } from '$app/environment';
+        import Crisp from './components/Crisp.svelte';
 	import Nightmare from '../components/Nightmare.svelte';
 
-        if (browser) window.$crisp=[];window.CRISP_WEBSITE_ID="6afed643-3d77-491b-879b-ff1098026756";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-	
         export let data;
 </script>
 
 <Header user={data.user || null} />
+
+{#if data.user}
+    <Crisp nickname={data.user.discordUser.username} avatar="https://cdn.discordapp.com/avatars/{data.user.id}/{data.user.discordUser.avatar}" />
+{/if}
 
 <Nightmare
 	Title="Home"
