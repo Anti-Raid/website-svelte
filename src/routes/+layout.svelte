3286ead27503d9ede37fe692c8585001112b69ac
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-24 17:34:24 -0600

    Fixed (maybe)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
 	import '../app.css';
 	import 'sweetalert2/dist/sweetalert2.min.css';
 	import Header from '../components/Header.svelte';
-
+        import { browser } from '$app/environment';
 	import Nightmare from '../components/Nightmare.svelte';
 
-        window.$crisp=[];window.CRISP_WEBSITE_ID="6afed643-3d77-491b-879b-ff1098026756";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+        if (browser) window.$crisp=[];window.CRISP_WEBSITE_ID="6afed643-3d77-491b-879b-ff1098026756";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
 	
         export let data;
 </script>
