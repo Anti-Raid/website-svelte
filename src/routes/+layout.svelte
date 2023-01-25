f9acf7afc3c9aacafa187d580282c0c6d12a5c68
selectdev <66577268+selectdev@users.noreply.github.com>
2023-01-25 08:49:00 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,12 @@
 	import '../app.css';
 	import 'sweetalert2/dist/sweetalert2.min.css';
 	import Header from '../components/Header.svelte';
-        import Crisp from '../components/Crisp.svelte';
-	import Nightmare from '../components/Nightmare.svelte';
+        import Nightmare from '../components/Nightmare.svelte';
 
         export let data;
 </script>
 
 <Header user={data.user || null} />
-
-{#if data.user}
-   <Crisp nickname={data.user.discordUser.username} avatar="https://cdn.discordapp.com/avatars/{data.user.id}/{data.user.discordUser.avatar}" />
-{/if}
 
 <Nightmare
 	Title="Home"
