9fb7b7bd66e66fa91cecb30d26bbdb22ba2637ee
selectdev <66577268+selectdev@users.noreply.github.com>
2023-07-17 11:58:09 -0500

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -93,13 +93,13 @@
 	long="This website is experimental, and may have issues."
 />
 
-<nav class="bg-gray-800">
+<nav>
 	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 		<div class="relative flex h-16 items-center justify-between">
 			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
 				<button
 					type="button"
-					class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+					class="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 					on:click={openMobileMenu}
 					aria-controls="mobile-menu"
 					aria-expanded="false"
@@ -149,7 +149,7 @@
 								class={classNames(
 									item.current
 										? 'bg-gray-900 text-white'
-										: 'text-gray-300 hover:bg-gray-700 hover:text-white',
+										: 'text-white hover:bg-gray-300',
 									'px-3 py-2 rounded-md text-sm font-medium'
 								)}
 								aria-current={item.current ? 'page' : undefined}
