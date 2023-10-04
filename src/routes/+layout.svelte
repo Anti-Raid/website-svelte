9d928cfc1a7dafb98ceabcd6c74c0deecac035b1
selectdev <66577268+selectdev@users.noreply.github.com>
2023-10-04 14:54:55 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,20 @@
 	import Header from '../components/Header.svelte';
 	import Nightmare from '../components/Nightmare.svelte';
 
+	onMount(async () => {
+		// Sentry
+		const Sentry = await import('@sentry/browser');
+
+		Sentry.init({
+			dsn: 'https://77411f26652c6ae892ae07782d762b74@trace.select-list.xyz/6',
+			replaysSessionSampleRate: 0.3,
+			tracesSampleRate: 0.4,
+			replaysOnErrorSampleRate: 1.0,
+			integrations: [new Sentry.Replay()],
+			release: `ar_web@0877309`
+		});
+	});
+
 	export let data;
 </script>
 
