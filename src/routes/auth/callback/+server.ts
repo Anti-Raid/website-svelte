6e389f5f668cdd84b62de68797112f88a2d749e0
selectdev <selectdev01@gmail.com>
2023-11-25 22:24:45 +0000

    r

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -3,7 +3,7 @@ import { error, redirect } from '@sveltejs/kit';
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ request, cookies, url }) {
 	const token = url.searchParams.get('token') || null;
-    const discordToken = url.searchParams.get("discord") || null;
+    const discordToken = url.searchParams.get('discord') || null;
 
 	if (!token) throw error(400, 'No authentication token was passed with this request.');
     if (!discordToken) throw error(400, 'No Discord token was passed with this request.');
