ad434ac0c10416f36f311edd499edc79e29974f5
Sanandan Sashikumar <cheesycod@outlook.com>
2024-08-26 07:23:25 +0000

    fix

--- src/components/inputs/button/tabs/TabbedPane.svelte
+++ src/components/inputs/button/tabs/TabbedPane.svelte
@@ -17,22 +17,20 @@
 	}
 </script>
 
-<center>
-	<div class="btn-group variant-filled text-sm rounded-md">
-		{#each tabs as tab}
-			<TabButton
-				visible={tab.id == visibleTab}
-				text={tab.label}
-				onClick={() => {
-					if (tab.onClick) {
-						tab.onClick();
-						return;
-					} else {
-						logger.info('TabButton.switch', tab.id);
-						visibleTab = tab.id;
-					}
-				}}
-			/>
-		{/each}
-	</div>
-</center>
+<div class="btn-group variant-filled text-sm rounded-md">
+	{#each tabs as tab}
+		<TabButton
+			visible={tab.id == visibleTab}
+			text={tab.label}
+			onClick={() => {
+				if (tab.onClick) {
+					tab.onClick();
+					return;
+				} else {
+					logger.info('TabButton.switch', tab.id);
+					visibleTab = tab.id;
+				}
+			}}
+		/>
+	{/each}
+</div>
