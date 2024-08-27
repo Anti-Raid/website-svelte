3f3c303cf1929ae1cb2f8c4932f0151f5b933e18
Select <66577268+selectdev@users.noreply.github.com>
2024-08-27 13:47:42 -0500

    Update Benefit.svelte

--- src/components/Benefit.svelte
+++ src/components/Benefit.svelte
@@ -5,16 +5,16 @@
 	export let title: string;
 </script>
 
-<div class="relative px-3">
+<div class="relative px-5">
 	<dt>
 		<div
 			class="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white"
 		>
 			<Icon {icon} class="text-2xl" />
 		</div>
-		<p class="ml-16 text-lg font-medium leading-6 text-white">{title}</p>
+		<p class="ml-16 text-lg font-cursive font-bold leading-6 text-white">{title}</p>
 	</dt>
-	<dd class="mt-2 ml-16 text-base text-white">
+	<dd class="mt-2 ml-16 text-base text-white text-monster">
 		<slot />
 	</dd>
 </div>
