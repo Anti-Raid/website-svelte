9e1bc9b0e83652b9fea2014a6b64e4fb9fb35116
Select <66577268+selectdev@users.noreply.github.com>
2024-08-28 12:31:58 -0500

    Update Themer.svelte

--- src/components/Themer.svelte
+++ src/components/Themer.svelte
@@ -5,16 +5,16 @@
 	export let isOpen: boolean = false;
 
 	const themes = [
-		{ id: 'rocket', color: 'rocket', label: 'Rocket', class: 'text-primary-500' },
-		{ id: 'skeleton', color: 'skeleton', label: 'Skeleton', class: 'text-primary-500' },
-		{ id: 'wintry', color: 'wintry', label: 'Wintry', class: 'text-primary-500' },
-		{ id: 'modern', color: 'modern', label: 'Modern', class: 'text-primary-500' },
-		{ id: 'seafoam', color: 'seafoam', label: 'Seafoam', class: 'text-primary-500' },
-		{ id: 'vintage', color: 'vintage', label: 'Vintage', class: 'text-primary-500' },
-		{ id: 'sahara', color: 'sahara', label: 'Sahara', class: 'text-primary-500' },
-		{ id: 'hamlindigo', color: 'hamlindigo', label: 'Hamlindigo', class: 'text-primary-500' },
-		{ id: 'gold-nouveau', color: 'gold-nouveau', label: 'Gold Nouveau', class: 'text-primary-500' },
-		{ id: 'crimson', color: 'crimson', label: 'Crimson', class: 'text-primary-500' }
+		{ id: 'rocket', color: 'rocket', label: 'Rocket', class: 'text-white' },
+		{ id: 'skeleton', color: 'skeleton', label: 'Skeleton', class: 'text-white' },
+		{ id: 'wintry', color: 'wintry', label: 'Wintry', class: 'text-white' },
+		{ id: 'modern', color: 'modern', label: 'Modern', class: 'text-white' },
+		{ id: 'seafoam', color: 'seafoam', label: 'Seafoam', class: 'text-white' },
+		{ id: 'vintage', color: 'vintage', label: 'Vintage', class: 'text-white' },
+		{ id: 'sahara', color: 'sahara', label: 'Sahara', class: 'text-white' },
+		{ id: 'hamlindigo', color: 'hamlindigo', label: 'Hamlindigo', class: 'text-white' },
+		{ id: 'gold-nouveau', color: 'gold-nouveau', label: 'Gold Nouveau', class: 'text-white' },
+		{ id: 'crimson', color: 'crimson', label: 'Crimson', class: 'text-white' }
 	];
 
 	// Temporary
@@ -53,15 +53,15 @@
 								: 'text-white/75 hover:text-white/100 hover:bg-gray-800/20'
 						} `}
 					>
-						<div data-theme={th.label} class="flex items-center justify-between w-full">
-							<span class={`hover:bg-${String(th.color)}-500`}>
+						<div data-theme={th.color} class="flex items-center justify-between w-full">
+							<span class={`text-white hover:bg-${String(th.color)}-500`}>
 								{th.label}
 							</span>
 							<Icon
 								icon="ic:round-circle"
 								class={`${
 									theme === th.id ? 'border-white dark:border-black' : 'border-black/0'
-								} border-2 rounded-full ${th.class} mr-1`}
+								} border-2 rounded-full bg-primary-500/80 mr-1`}
 							/>
 						</div>
 					</button>
