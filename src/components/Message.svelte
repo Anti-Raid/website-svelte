c2bf5d2950ecb4c7f7e47abc0019a0b117ca8ac5
Select <66577268+selectdev@users.noreply.github.com>
2024-09-09 05:13:08 -0500

    Update Message.svelte

--- src/components/Message.svelte
+++ src/components/Message.svelte
@@ -3,54 +3,28 @@
 
 	export let big = false;
 	export let type: 'loading' | 'info' | 'error' | 'success';
+
+        let icon = '';
+        let color = '';
+        if (type === 'loading') {
+           icon = 'loading';
+           color = 'white';
+        }
+        if (type === 'info') {
+           icon = 'information';
+           color = 'blue'
+        }
+        if (type === 'error') {
+           icon = 'alert';
+           color = 'red';
+        }
+        if (type === 'success') {
+           icon = 'check';
+           color = 'green';
+        }
 </script>
 
-{#if type == 'loading'}
-	{#if big}
-		<div class="text-white text-4xl text-center">
-			<Icon icon="mdi:loading" class="animate-spin block text-center m-auto" />
-			<slot />
-		</div>
-	{:else}
-		<div class="text-white text-2xl">
-			<Icon icon="mdi:loading" class="animate-spin inline-block text-2xl" />
-			<slot />
-		</div>
-	{/if}
-{:else if type == 'info'}
-	{#if big}
-		<div class="text-blue-500 text-4xl text-center">
-			<Icon icon="mdi:information" class="inline-block text-2xl" />
-			<slot />
-		</div>
-	{:else}
-		<div class="text-blue-500 text-2xl">
-			<Icon icon="mdi:information" class="inline-block text-3xl align-top" />
-			<slot />
-		</div>
-	{/if}
-{:else if type == 'error'}
-	{#if big}
-		<div class="text-red-500 text-4xl text-center">
-			<Icon icon="mdi:alert" class="inline-block text-2xl" />
-			<slot />
-		</div>
-	{:else}
-		<div class="text-red-500 text-2xl">
-			<Icon icon="mdi:alert" class="inline-block text-3xl align-top" />
-			<slot />
-		</div>
-	{/if}
-{:else if type == 'success'}
-	{#if big}
-		<div class="text-green-500 text-4xl text-center">
-			<Icon icon="mdi:check" class="inline-block text-2xl" />
-			<slot />
-		</div>
-	{:else}
-		<div class="text-green-500 text-2xl">
-			<Icon icon="mdi:check" class="inline-block text-3xl align-top" />
-			<slot />
-		</div>
-	{/if}
-{/if}
+<div class="{color === 'white' ? 'text-white' : 'text-{color}-500'} text-{big ? '4xl text-center': '2xl'}">
+   <Icon icon="mdi:{icon}" class="animate-spin {big ? 'block m-auto ' : 'inline-block text-2xl'}" />
+   <slot />
+</div>
