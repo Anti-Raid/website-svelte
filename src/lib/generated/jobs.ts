4ca8e7276d9099f92a8e3a9405c12e1b1d8296dc
Sanandan Sashikumar <cheesycod@outlook.com>
2024-09-24 02:17:57 +0000

    fix

--- src/lib/generated/jobs.ts
+++ src/lib/generated/jobs.ts
@@ -3,50 +3,50 @@
 //////////
 // source: types.go
 
-export interface TaskCreateResponse {
+export interface JobCreateResponse {
   id: string;
 }
 /**
- * @ci table=tasks
+ * @ci table=jobs
  * Jobs are background processes that can be run on a coordinator server.
  */
-export interface Task {
+export interface Job {
   id: string;
   name: string;
-  output?: TaskOutput;
+  output?: Output;
   fields: { [key: string]: any};
   statuses: { [key: string]: any}[];
-  task_for?: TaskFor;
+  owner?: Owner;
   expiry?: any /* time.Duration */;
   state: string;
   resumable: boolean;
   created_at: string /* RFC3339 */;
 }
 /**
- * @ci table=tasks unfilled=1
- * A PartialTask represents a partial representation of a job.
+ * @ci table=jobs unfilled=1
+ * A PartialJob represents a partial representation of a job.
  */
-export interface PartialTask {
+export interface PartialJob {
   id: string;
   name: string;
   expiry?: any /* time.Duration */;
   state: string;
   created_at: string /* RFC3339 */;
 }
-export interface TaskListResponse {
-  tasks: PartialTask[];
+export interface JobListResponse {
+  jobs: PartialJob[];
 }
 /**
- * TaskFor is a struct containing the internal representation of who a task is for
+ * Owner is a struct containing the internal representation of who a job is for
  */
-export interface TaskFor {
+export interface Owner {
   id: string;
   target_type: string;
 }
 /**
- * TaskOutput is the output of a task
+ * Output is the output of a job
  */
-export interface TaskOutput {
+export interface Output {
   filename: string;
-  segregated: boolean; // If this flag is set, then the stored output will be stored in $taskForSimplexFormat/$Name/$taskId/$filename instead of $taskId/$filename
+  segregated: boolean; // If this flag is set, then the stored output will be stored in $jobForSimplexFormat/$Name/$id/$filename instead of $id/$filename
 }
