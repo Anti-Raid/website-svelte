e441c931bf44b9219a9c8e348199662c0304af11
selectdev <selectdev01@gmail.com>
2024-09-29 07:57:47 +0000

    uhhhhhh

--- src/routes/forums/+page.svelte
+++ src/routes/forums/+page.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
 	import { PageData } from './$types';
-    import Debug from '../../components/common/Debug.svelte';
-    import CreatePost from '../../components/forums/CreatePost.svelte';
+	import Debug from '../../components/common/Debug.svelte';
+	import CreatePost from '../../components/forums/CreatePost.svelte';
+	import PostCard from '../../components/forums/PostCard.svelte';
 
 	export let data: PageData;
 </script>
 
 {#if data.token}
-    {#if data.user}
-        {#if data.user.state === "ACTIVE"}
-            <CreatePost Token={data.token} />
-        {/if}
-    {/if}
+	{#if data.user}
+		{#if data.user.state === 'ACTIVE'}
+			<CreatePost Token={data.token} />
+		{/if}
+	{/if}
 {/if}
 
-<Debug data={data} />
+{#if data.posts}
+	{#each data.posts as post}
+		<PostCard User={post.user} Post={post} AuthedUser={data.user} Token={data.token} />
+	{/each}
+{/if}
+
+<Debug {data} />
