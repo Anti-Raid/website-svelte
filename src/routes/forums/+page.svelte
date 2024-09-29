4d681c8e7c0798c76fe321e967bb372af06692e9
selectdev <selectdev01@gmail.com>
2024-09-29 07:45:29 +0000

    cum

--- src/routes/forums/+page.svelte
+++ src/routes/forums/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import { PageData } from './$types';
     import Debug from '../../components/common/Debug.svelte';
+    import CreatePost from '../../components/forums/CreatePost.svelte';
 
 	export let data: PageData;
 </script>
+
+{#if data.token}
+    {#if data.user}
+        {#if data.user.state === "ACTIVE"}
+            <CreatePost Token={data.token} />
+        {/if}
+    {/if}
+{/if}
 
 <Debug data={data} />
