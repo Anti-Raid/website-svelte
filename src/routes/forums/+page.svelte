01839659803c163a54e091599a38d7e1a60dcde9
selectdev <selectdev01@gmail.com>
2024-09-29 07:37:44 +0000

    fix

--- src/routes/forums/+page.svelte
+++ src/routes/forums/+page.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
 	import { PageData } from './$types';
+    import Debug from '../../components/common/Debug.svelte';
 
 	export let data: PageData;
-	let i = JSON.stringify(data, null, 4);
 </script>
 
-<textarea
-	id="editor"
-	rows="8"
-	class="block w-full px-2 text-sm rounded-md text-white bg-slate-600 font-cabin font-semibold border-0 focus:ring-0"
-	placeholder="Sorry, no data was recieved from the server."
-	contenteditable="false"
-	bind:textContent={i}
-	required
-/>
+<Debug data={data} />
