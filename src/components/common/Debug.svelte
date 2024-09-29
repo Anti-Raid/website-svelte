1dc1a2e00bf0d097d977a8568792a3d8a192d646
selectdev <selectdev01@gmail.com>
2024-09-29 07:12:25 +0000

    fix

--- src/components/common/Debug.svelte
+++ src/components/common/Debug.svelte
@@ -6,13 +6,16 @@
 </script>
 
 <Developer>
-	<textarea
-		id="editor"
-		rows="8"
-		class="block w-full px-2 text-sm rounded-md text-white bg-slate-600 font-cabin font-semibold border-0 focus:ring-0"
-		placeholder="Sorry, no data was recieved from the server."
-		contenteditable="false"
-		bind:textContent={i}
-		required
-	/>
+	<details>
+		<summary class="hover:cursor-pointer">Debug</summary>
+		<textarea
+			id="editor"
+			rows="8"
+			class="block w-full px-2 text-sm rounded-md text-white bg-slate-600 font-cabin font-semibold border-0 focus:ring-0"
+			placeholder="Sorry, no data was recieved from the server."
+			contenteditable="false"
+			bind:textContent={i}
+			required
+		/>
+	</details>
 </Developer>
