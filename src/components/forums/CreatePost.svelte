95ed85c45035a467882d0e411f55861cfde85453
Select <66577268+selectdev@users.noreply.github.com>
2024-10-03 15:21:51 -0500

    Update CreatePost.svelte

--- src/components/forums/CreatePost.svelte
+++ src/components/forums/CreatePost.svelte
@@ -165,12 +165,12 @@
 <input
 	placeholder="Caption*"
 	name="caption"
-	class="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-3 bg-surface-800 text-primary-600 font-bold placeholder-primary-400 focus:z-10 focus:border-gray-400 focus:outline-none focus:ring-surface-500 sm:text-sm"
+	class="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-3 bg-surface-800 text-primary-500 font-bold placeholder-primary-400 focus:z-10 focus:border-gray-400 focus:outline-none focus:ring-surface-500 sm:text-sm"
 	bind:value={Caption}
 />
 
 <div
-	class="flex w-full appearance-none rounded-b-md border border-gray-300 px-3 py-6 bg-surface-800 text-primary-600 font-bold focus:z-10 focus:border-gray-400 focus:outline-none focus:ring-surface-500 sm:text-sm"
+	class="flex w-full appearance-none rounded-b-md border border-gray-300 px-3 py-6 bg-surface-800 text-primary-500 font-bold focus:z-10 focus:border-gray-400 focus:outline-none focus:ring-surface-500 sm:text-sm"
 >
 	<button type="button" on:click={uploadImage}>
 		<svg
@@ -214,7 +214,7 @@
 			viewBox="0 0 24 24"
 			stroke-width="1.5"
 			stroke="currentColor"
-			class="w-6 h-6"
+			class="w-6 h-6 text-primary-400"
 		>
 			<path
 				stroke-linecap="round"
@@ -233,7 +233,7 @@
 			viewBox="0 0 24 24"
 			stroke-width="1.5"
 			stroke="currentColor"
-			class="w-6 h-6"
+			class="w-6 h-6 text-primary-400"
 		>
 			<path
 				stroke-linecap="round"
