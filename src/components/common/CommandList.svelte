df2be9502acd6e1f1980d56a0f777adf0cf1260c
Sanandan Sashikumar <cheesycod@outlook.com>
2024-12-26 09:21:58 +0000

    bug fix

--- src/components/common/CommandList.svelte
+++ src/components/common/CommandList.svelte
@@ -15,6 +15,7 @@
 	import Debug from './Debug.svelte';
 	import { CanonicalCommand } from '@lib/generated/silverpelt';
 	import { BotState } from '@lib/generated/types';
+	import { assert } from 'console';
 
 	interface State {
 		commandSearch: string;
@@ -28,19 +29,33 @@
 		searchedCommands: []
 	};
 
+	let cmdDataTable: Readable<CanonicalCommand[]>;
+	let handler: DataHandler<CanonicalCommand>;
 	const fetchBotState = async () => {
 		logger.info('FetchBotState', 'Fetching bot state');
 		state.botState = await makeSharedRequest(opGetBotState());
 		state.botState.commands = parseCommands(state.botState.commands);
+
+		handler = new DataHandler(state.botState.commands, { rowsPerPage: 10 });
+		cmdDataTable = handler.getRows();
+
+		if (!cmdDataTable || $cmdDataTable.length === 0) {
+			throw new Error('Failed to load commands');
+		}
+
 		return state.botState;
 	};
 
 	const parseCommands = (commands: CanonicalCommand[], base?: string): CanonicalCommand[] => {
+		logger.info('ParseCommands', 'Parsing commands');
 		let parsedCommands: CanonicalCommand[] = [];
 
 		for (let command of commands) {
 			if (command.subcommands.length > 0) {
-				parsedCommands.push(...parseCommands(command.subcommands, command.name));
+				parsedCommands.push(command);
+				parsedCommands.push(
+					...parseCommands(command.subcommands, command.name || command.qualified_name)
+				);
 			} else {
 				parsedCommands.push({
 					...command,
@@ -59,22 +74,6 @@
 	} else {
 		state.searchedCommands = [];
 	}
-
-	let cmdDataTable: Readable<CanonicalCommand[]>;
-
-	const createCmdDataTable = async () => {
-		while (!state.botState) {
-			// Wait for 500ms using Promise
-			await new Promise((resolve) => setTimeout(resolve, 500));
-		}
-		const handler = new DataHandler(state.botState.commands, { rowsPerPage: 20 });
-		cmdDataTable = handler.getRows();
-
-		return {
-			handler,
-			rows: cmdDataTable
-		};
-	};
 </script>
 
 <!--Search bar-->
@@ -105,118 +104,100 @@
 		{#await fetchBotState()}
 			<Message type="loading">Loading bot state...</Message>
 		{:then botState}
-			{#await createCmdDataTable()}
-				<Message type="loading">Loading commands...</Message>
-			{:then data}
-				<div class="overflow-x-auto space-y-4">
-					<!-- Header -->
-					<header class="flex justify-between gap-4">
-						<Search handler={data.handler} />
-						<RowsPerPage handler={data.handler} />
-					</header>
+			<div class="overflow-x-auto space-y-4">
+				<!-- Header -->
+				<header class="flex justify-between gap-4">
+					<Search {handler} />
+					<RowsPerPage {handler} />
+				</header>
 
-					<!-- Table -->
-					<table class="table table-hover table-compact bg-surface-600 w-full table-auto">
-						<thead>
-							<tr class="bg-surface-800">
-								<ThSort handler={data.handler} orderBy={'qualified_name'}>Name</ThSort>
-								<ThSort handler={data.handler} orderBy={'description'}>Description</ThSort>
-								<ThSort handler={data.handler} orderBy={'arguments'}>Arguments</ThSort>
-								<ThSort handler={data.handler} orderBy={'qualified_name'}>Permissions</ThSort>
-							</tr>
-							<tr class="bg-surface-800">
-								<ThFilter handler={data.handler} filterBy={'qualified_name'} what="Name" />
-								<ThFilter handler={data.handler} filterBy={'description'} what="Description" />
-								<ThFilter handler={data.handler} filterBy={'arguments'} what="Arguments" />
-								<ThFilter handler={data.handler} filterBy={'qualified_name'} what="Permissions" />
-							</tr>
-						</thead>
+				<!-- Table -->
+				<table class="table table-hover table-compact bg-surface-600 w-full table-auto">
+					<thead>
+						<tr class="bg-surface-800">
+							<ThSort {handler} orderBy={'qualified_name'}>Name</ThSort>
+							<ThSort {handler} orderBy={'description'}>Description</ThSort>
+							<ThSort {handler} orderBy={'arguments'}>Arguments</ThSort>
+							<ThSort {handler} orderBy={'command_permissions'}>Permissions</ThSort>
+						</tr>
+						<tr class="bg-surface-800">
+							<ThFilter {handler} filterBy={'qualified_name'} what="Name" />
+							<ThFilter {handler} filterBy={'description'} what="Description" />
+							<ThFilter {handler} filterBy={'arguments'} what="Arguments" />
+							<ThFilter {handler} filterBy={'command_permissions'} what="Permissions" />
+						</tr>
+					</thead>
 
-						<tbody class="bg-surface-500 text-white">
-							{#each $cmdDataTable as row}
-								<tr>
-									<td>
-										{#if !row.qualified_name.includes(' ')}
-											<span class="font-semibold">
-												{row.name}
-											</span>
+					<tbody class="bg-surface-500 text-white">
+						{#each $cmdDataTable as row}
+							<tr>
+								<td>
+									<span class="font-semibold">
+										{row.qualified_name}
+									</span>
+
+									<!--NSFW command, TODO: Make tooltip-->
+									{#if row.nsfw}
+										<div class="command-note">
+											<span class="text-red-400 font-semibold">NSFW</span>
+										</div>
+									{/if}
+
+									<!--Base command of a slash command, TODO: Make tooltip-->
+									{#if row.subcommand_required || row.subcommands.length}
+										<div class="command-note">
+											<span class="text-blue-400 font-semibold">BASE</span>
+										</div>
+									{/if}
+								</td>
+								<td>
+									<span>
+										{#if row.description}
+											{row.description}
 										{:else}
-											<span class="whitespace-nowrap">
-												<span class="font-semibold">{row?.qualified_name?.split(' ')[0]}</span
-												>{' '}<em
-													>{row?.qualified_name?.replace(
-														row?.qualified_name?.split(' ')[0] + ' ',
-														''
-													)}</em
-												>
-											</span>
+											Mystery Box?
 										{/if}
-
-										<!--NSFW command, TODO: Make tooltip-->
-										{#if row.nsfw}
-											<div class="command-note">
-												<span class="text-red-400 font-semibold">NSFW</span>
-											</div>
-										{/if}
-
-										<!--Base command of a slash command, TODO: Make tooltip-->
-										{#if row.subcommand_required || row.subcommands.length}
-											<div class="command-note">
-												<span class="text-blue-400 font-semibold">BASE</span>
-											</div>
-										{/if}
-									</td>
-									<td>
-										<span>
-											{#if row.description}
-												{row.description}
-											{:else}
-												Mystery Box?
-											{/if}
-										</span>
-									</td>
-									<td>
-										<span>
-											<ul class="list-disc list-outside text-white">
-												{#each row.arguments as arg, i}
-													<li class={i + 1 < row.arguments.length ? 'mb-2' : ''}>
-														<span class="font-semibold">{arg.name}</span>
-
-														{#if arg.required}
-															<span class="text-red-400 font-semibold">*</span>
-														{/if}
-
-														{#if arg.description}
-															<span>: </span><em>{arg.description}</em>
-														{/if}
-													</li>
-												{/each}
-											</ul>
-										</span>
-									</td>
-									<td>
+									</span>
+								</td>
+								<td>
+									<span>
 										<ul class="list-disc list-outside text-white">
-											{#each botState.command_permissions[row.qualified_name] || [] as permDesc}
-												<li class="mb-2">
-													<span class="font-semibold">{permDesc}</span>
+											{#each row.arguments || [] as arg, i}
+												<li class={i + 1 < row.arguments.length ? 'mb-2' : ''}>
+													<span class="font-semibold">{arg.name}</span>
+
+													{#if arg.required}
+														<span class="text-red-400 font-semibold">*</span>
+													{/if}
+
+													{#if arg.description}
+														<span>: </span><em>{arg.description}</em>
+													{/if}
 												</li>
 											{/each}
 										</ul>
-									</td>
-								</tr>
-							{/each}
-						</tbody>
-					</table>
+									</span>
+								</td>
+								<td>
+									<ul class="list-disc list-outside text-white">
+										{#each botState.command_permissions[row.qualified_name] || [] as permDesc}
+											<li class="mb-2">
+												<span class="font-semibold">{permDesc}</span>
+											</li>
+										{/each}
+									</ul>
+								</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
 
-					<!-- Footer -->
-					<footer class="flex justify-between">
-						<RowCount handler={data.handler} />
-						<Pagination handler={data.handler} />
-					</footer>
-				</div>
-			{:catch}
-				<Message type="error">Failed to load commands</Message>
-			{/await}
+				<!-- Footer -->
+				<footer class="flex justify-between">
+					<RowCount {handler} />
+					<Pagination {handler} />
+				</footer>
+			</div>
 		{:catch}
 			<Message type="error">Failed to load modules</Message>
 		{/await}
